85f7521e3176da317f7552b9c9d8b0792566dc9e
YashIyengar <iyengar2589@gmail.com>
2021-12-26 17:04:36 +0000

    sending data via smtp

--- scraper.py
+++ scraper.py
@@ -1,69 +1,23 @@
 import requests
 from bs4 import BeautifulSoup  
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import pandas as pd
-
-
 YOUTUBE_TRENDNG_URL = 'https://www.youtube.com/feed/trending'
 
 
-def get_driver():
-  chrome_options = Options()
-  chrome_options.add_argument('--no-sandbox')
-  chrome_options.add_argument('--disable-dev-shm-usage')
-  chrome_options.add_argument('--headless')
-  driver = webdriver.Chrome(options= chrome_options)
-  return driver
+# Does not execute Javascript
+response = requests.get(YOUTUBE_TRENDNG_URL)
 
-def get_videos(driver):
-  VIDEO_DIVS_TAG = 'ytd-video-renderer'
-  driver.get(YOUTUBE_TRENDNG_URL)
-  videos = driver.find_elements(By.TAG_NAME,VIDEO_DIVS_TAG)
-  return videos
-
-def parse_video(video):
-  #title, url, thumbnail_url, channel, views, uploaded, description
-  title_tag = video.find_element(By.ID, 'video-title')
-  title = title_tag.text
-  
-  url = title_tag.get_attribute('href')
-  
-  thumbnail_tag = video.find_element(By.TAG_NAME,'img')
-  thumbnail_url = thumbnail_tag.get_attribute('src')
-
-  channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
-  channel_name = channel_div.text
-
-  description = video.find_element(By.ID, 'description-text').text
-  return {
-    "title":title,
-    "url":url,
-    "thumbnail_url":thumbnail_url,
-    'channel': channel_name,
-    'description':description
-
-  }
+print('status code',response.status_code)
 
 
-if __name__ == "__main__":
-  print("Creating Driver...")
-  driver = get_driver()
-  
-  print('Fetching trending videos...')
-  videos = get_videos(driver)
+with open('trending.html', 'w') as f:
+  f.write(response.text)
 
-  print(f'Found {len(videos)} videos')
+doc = BeautifulSoup(response.text, 'html.parser')
 
-  print('Parsing the top 10 videos ')
-  videos_data = [parse_video(video) for video in videos[:10]]
+print("Page title:", doc.title.text)
 
-  print("Save the data to CSV")
-  videos_df = pd.DataFrame(videos_data)
-  print(videos_df)
-  videos_df.to_csv("trending.csv", index=None)
+# Find all the video ZeroDivisionError
 
-  
+video_divs = doc.find_all('div', class_='ytd-video-renderer')
 
-  
+print(f'Found {len(video_divs)} videos')
